ab839dff66026be3301f9f1ef0a714d8b9a29f83
sackvo8z <sackvo8z@gmail.com>
2024-02-07 08:57:50 +0700

    migrate admin

--- database/migrations/2024_02_06_174643_create_admins_table.php
+++ database/migrations/2024_02_06_174643_create_admins_table.php
@@ -1,8 +1,9 @@
 <?php
 
-use Illuminate\Database\Migrations\Migration;
-use Illuminate\Database\Schema\Blueprint;
+use App\Enums\AdminRoleEnum;
 use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
 
 return new class extends Migration
 {
@@ -13,6 +14,12 @@ return new class extends Migration
     {
         Schema::create('admins', function (Blueprint $table) {
             $table->id();
+            $table->string('name');
+            $table->string('email')->unique();
+            $table->timestamp('email_verified_at')->nullable();
+            $table->string('password');
+            $table->string('role')->default(AdminRoleEnum::SuperAdmin->value);
+            $table->rememberToken();
             $table->timestamps();
         });
     }
